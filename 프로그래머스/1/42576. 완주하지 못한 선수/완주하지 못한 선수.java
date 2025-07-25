e529f11748f6ffe09ead9ec4f2bbb09f6c228e48
seungwoo <fatmunk7504@gmail.com>
2025-07-25 17:24:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.81 ms, Memory: 96.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        // 1. 참가자 이름을 map에 저장 (등장 횟수 기록)
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p,0) + 1);
+        }
+        
+        // 2. 완주자 이름을 map에서 제거 (등장 횟수 감소)
+        for (String c : completion){
+            map.put(c, map.get(c) - 1);
+        }
+        
+        // 3. 값이 1로 남아 있는 이름이 완주 못한 사람
+        for (String key : map.keySet()){
+            if (map.get(key) != 0){
+                return key;
+            }
+        }
+        return "";
+    }
+}
