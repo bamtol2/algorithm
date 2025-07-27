f0fbbd4dfcbd38b5866f0f3893396ce86362dc82
seungwoo <fatmunk7504@gmail.com>
2025-07-27 18:31:20 +0900

    [level 2] Title: 전화번호 목록, Time: 319.55 ms, Memory: 263 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> set = new HashSet<>();
+        
+        for(String p : phone_book){
+            set.add(p);
+        }
+        
+        for(String p : phone_book){
+            for(int i = 0; i < p.length(); i++){
+                String prefix = p.substring(0,i);
+                if(set.contains(prefix)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
