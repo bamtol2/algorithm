c867d9070758fba7e5153130cac16e31ed375d91
seungwoo <fatmunk7504@gmail.com>
2025-08-03 14:25:58 +0900

    [level 3] Title: 단어 변환, Time: 0.92 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Node{
+class Node {
     String word;
     int step;
     
@@ -12,15 +12,14 @@ class Node{
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        
-        // 바로 종료되는 조건
+        // words 배열에 target이 없으면 바로 0 반환
         if (!Arrays.asList(words).contains(target)){
             return 0;
         }
         
-        int n = words.length;
-        boolean[] visited = new boolean[n];
-        Queue<Node> queue = new LinkedList<>();
+        int n = words.length; // words 배열의 길이
+        boolean[] visited = new boolean[n]; // 방문 배열 선언
+        Queue<Node> queue = new LinkedList<>(); // 단어랑 단계를 함께 담기 위해 Node로 큐 생성
         queue.offer(new Node(begin,0));
         
         while(!queue.isEmpty()){
@@ -31,24 +30,28 @@ class Solution {
                 return current.step;
             }
             
+            // words를 돌면서
             for (int i = 0; i < n; i++){
-                if(!visited[i]){
-                    String compare = words[i];
+                // 방문하지 않았으면
+                if (!visited[i]){
                     int diff = 0;
+                    String compare = words[i]; // 그 단어를 꺼내서
+                    // 한글자씩 비교
                     for (int j = 0; j < compare.length(); j++){
+                        // 서로 다른 두 단어의 글자가 다르다면
                         if (current.word.charAt(j) != compare.charAt(j)){
                             diff++;
                         }
                     }
-                    // 단어가 한글자 빼고 같다면
+                    // 위의 for문을 돌리고 1개의 글자만 다르다면 
                     if (diff == 1){
-                        visited[i] = true;
+                        visited[i] = true; //방문 처리 해주고
                         queue.offer(new Node(compare, current.step + 1));
+                        
                     }
                 }
             }
-            
         }
-        return 0;
+        return 0; // while문이 다 끝났는데도 값을 반환하지못했으면 0 반환
     }
 }
