9e73466d24deba33bbbc74c0ea5204160e80f6be
seungwoo <fatmunk7504@gmail.com>
2025-08-07 22:14:37 +0900

    [level 3] Title: 입국심사, Time: 59.04 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,34 +3,37 @@ class Solution {
         long left = 1;
         long right = 0;
         
-        // 가장 많이 걸리는 시간 계산
-        for (long time : times){
+        // 시간을 돌면서 가장 시간이 많이 걸리는 곳을 찾는다
+        for (int time : times){
             if (right < time){
                 right = time;
             }
         }
         
-        // 검사하는데 가장 많은 시간이 걸리는 시간 * n = 최대로 많이 걸리는 시간
-        right *= n;
+        // 가장 시간이 많이 걸리는 타임은 가장 많이 시간이 걸리는 곳 * 사람수
+        right *= n; 
         long answer = 0;
         
         while(left <= right){
             long mid = (left + right) / 2;
+            
             long people = 0;
             
-            // 그 시간동안 처리할 수 있는 사람의 수
-            for (long time : times){
+            // mid(최소시간) 동안 처리할 수 있는 사람의 수
+            for (int time : times){
                 people += mid / time;
             }
             
-            // 처리할 수 있는 사람수가 > 목표치(n)보다 크거나 같으면 -> 시간을 늘려줘야함
+            // 만약 처리할 수 있는 사람 수가 n 보다 적으면 시간이 적으므로 시간을 늘려야함
             if (people >= n){
-                answer = mid;
                 right = mid - 1;
-            } else { // 처리할 수 있는 사람수가 > 목표치(n) 보다 작으면 -> 시간을 줄여야함
+                answer = mid;
+            } else {
+                // 만약 처리할 수 있는 사람 수가 n 보다 많으면 시간이 많으므로 mid를 줄여야함
                 left = mid + 1;
             }
         }
+        
         return answer;
     }
 }
