d797c1f8d4a47b0a77121091594d3f5ef3e70b54
seungwoo <fatmunk7504@gmail.com>
2025-09-02 11:33:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,13 +1,13 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N,apartmentCount;
-	static int[][] arr;
+	static int N;
+	static int arr[][];
+	static List<Integer> apartment = new ArrayList<>();
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
@@ -20,40 +20,48 @@ public class Main {
 			}
 		}
 		
-		List<Integer> answer = new ArrayList<>();
-		int apartmentComplex = 0;
+		int apartmentCount = 0; //총 단지 수
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				apartmentCount = 0;
 				if (arr[i][j] == 1) {
-					dfs(i,j);
-					apartmentComplex++;
-					answer.add(apartmentCount);
+					arr[i][j] = 0;
+					int sum = bfs(i,j);
+					apartmentCount += 1;
+					apartment.add(sum);
 				}
 			}
 		}
 		
-		Collections.sort(answer);
+		System.out.println(apartmentCount);
 		
-		System.out.println(apartmentComplex);
+		Collections.sort(apartment);
 		
-		for (int a : answer) {
+		for (int a : apartment) {
 			System.out.println(a);
 		}
+		
 	}
 	
-	public static void dfs(int x, int y) {
-		arr[x][y] = 0;
-		apartmentCount++;
+	public static int bfs(int x, int y) {
+		Deque<int[]> deque = new ArrayDeque<>();
+		deque.addFirst(new int[] {x,y});
+		int sum = 1;
 		
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+		while (!deque.isEmpty()) {
+			int[] current = deque.removeLast();
 			
-			if (nx >= 0 && ny >= 0 && nx < N && ny < N && arr[nx][ny] == 1) {
-				dfs(nx,ny);
+			for (int i = 0; i < 4; i++) {
+				int nx = current[0] + dx[i];
+				int ny = current[1] + dy[i];
+				
+				if (nx >= 0 && ny >= 0 && nx < N && ny < N && arr[nx][ny] == 1) {
+					arr[nx][ny] = 0;
+					deque.addFirst(new int[] {nx,ny});
+					sum += 1;
+				}
 			}
 		}
+		return sum;
 	}
 }
