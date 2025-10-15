bf5d7466b72e384c100a9f07f051a12b32302bff
seungwoo <fatmunk7504@gmail.com>
2025-10-15 10:41:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.96 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        // String 키 , Integer 값 을 가지는 HashMap 생성
         HashMap<String,Integer> map = new HashMap<>();
         
-        // 1.참가자 이름을 map에 저장(등장 횟수 기록)
+        // 1. 참가자 이름을 map에 저장(등장 횟수 기록)
         for (String p : participant){
-            map.put(p, map.getOrDefault(p,0) + 1);
+            map.put(p,map.getOrDefault(p,0) + 1);
         }
         
-        // 2.완주자 이름을 map에서 제거(등장 횟수 감소)
+        // 2. 완주자 이름을 map에서 제거(등장 횟수 감소)
         for (String c : completion){
-            map.put(c, map.get(c)-1);
+            map.put(c,map.get(c)-1);
         }
         
-        // 3.값이 1로 남아 있는 이름이 완주 못한 사람
+        // 3. 값이 1로 남아 있는 이름이 완주 못한 사람
         for (String key : map.keySet()){
             if (map.get(key) == 1){
                 return key;
