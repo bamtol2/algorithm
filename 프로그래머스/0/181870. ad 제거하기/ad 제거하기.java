6371e7c4af8588610f2fec04cfdaae158fc4e714
seungwoo <fatmunk7504@gmail.com>
2025-12-07 21:49:39 +0900

    [level 0] Title: ad 제거하기, Time: 2.02 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(String[] strArr) {
+        int count = 0;
+        for (String s : strArr) {
+            if (!s.contains("ad")) count++;
+        }
+        
+        String[] answer = new String[count];
+        int idx = 0;
+        for (String s : strArr) {
+            if (!s.contains("ad")) {
+                answer[idx++] = s;
+            }
+        }
+        return answer;
+    }
+}
