a62ae99bc2171b1d013f69c0dceec95357feee72
seungwoo <fatmunk7504@gmail.com>
2026-01-01 22:06:00 +0900

    [level 0] Title: 접미사 배열, Time: 1.47 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String my_string) {
+        int n = my_string.length();
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = my_string.substring(i);
+        }
+        
+        java.util.Arrays.sort(answer);
+        return answer;
+    }
+}
