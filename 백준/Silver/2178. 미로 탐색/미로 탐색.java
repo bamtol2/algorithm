0f81ce619e4d554529eb94a56e48f15af9c310a8
seungwoo <fatmunk7504@gmail.com>
2026-01-06 10:13:48 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,54 +3,53 @@ import java.io.*;
 
 public class Main {
     static int N,M;
-    static int[][] map;
-    static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
+    static int[] dx = {0,0,-1,1};
+    static int[][] map;
     static boolean[][] visited;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // 세로
-        M = Integer.parseInt(st.nextToken()); // 가로
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        map = new int[N+1][M+1];
         visited = new boolean[N+1][M+1];
+        map = new int[N+1][M+1];
 
-        for(int i = 1; i <= N; i++){
-            String line = br.readLine();
+
+        for (int i = 1; i <= N; i++){
+            String a = br.readLine();
             for (int j = 1; j <= M; j++){
-                // charAt()의 결과는 문자(Char) 숫자로 쓰려면 - '0'
-                map[i][j] = line.charAt(j-1) - '0';
+                map[i][j] = a.charAt(j-1)-'0';
             }
         }
 
-        int answer = bfs(1,1);
-        System.out.println(answer);
+        bfs(1,1);
     }
 
-    static int bfs(int y, int x){
+    static void bfs(int y, int x){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{y,x});
         visited[y][x] = true;
 
         while(!q.isEmpty()){
-            int[] now = q.poll();
-            int nowY = now[0];
-            int nowX = now[1];
+            int[] cur = q.poll();
+            int curY = cur[0];
+            int curX = cur[1];
 
-            for(int i = 0; i < 4; i++){
-                int ny = nowY + dy[i];
-                int nx = nowX + dx[i];
-
+            for (int i = 0; i < 4; i++){
+                int ny = curY + dy[i];
+                int nx = curX + dx[i];
+                // 범위 안이고
                 if(ny > 0 && nx > 0 && ny <= N && nx <= M && !visited[ny][nx] && map[ny][nx] == 1){
-                    map[ny][nx] = map[nowY][nowX] + 1;
+                    map[ny][nx] = map[curY][curX] + 1;
                     visited[ny][nx] = true;
                     q.add(new int[]{ny,nx});
                 }
             }
         }
-        return map[N][M];
+        System.out.print(map[N][M]);
     }
 }
