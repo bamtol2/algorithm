0565bd495f4c83bb203d7d236b543d7d6e8843b7
seungwoo <fatmunk7504@gmail.com>
2026-01-07 11:14:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18968 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,51 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, K;
-    static int[] move = {-1, 1, 2};
-    static int[] count;
-    static Queue<Integer> q = new LinkedList<>();
+    static int N,K;
+    static int[] move = {1,-1,2};
+    static int[] number = new int[100001];
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        count = new int[100001];
-        Arrays.fill(count, -1);   // 방문 안 한 상태
+        Queue<Integer> q = new LinkedList<>();
 
-        count[N] = 0;             // 시작 위치: 이동 0번
         q.add(N);
+        number[N] = 1;
 
-        bfs();
+        bfs(q);
+
+        System.out.println(number[K] - 1);
+
     }
+    static void bfs(Queue<Integer> q){
+        while(!q.isEmpty()){
+            int cur = q.poll();
 
-    static void bfs() {
-        while (!q.isEmpty()) {
-            int now = q.poll();
-
-            if (now == K) {
-                System.out.println(count[now]);
-                return;
-            }
-
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++){
                 int next;
                 if (i == 2) {
-                    next = now * move[i];
+                    next = cur * 2;
                 } else {
-                    next = now + move[i];
+                    next = cur + move[i];
                 }
-
-                // 범위 체크
+                // 범위 확인
                 if (next < 0 || next >= 100001) continue;
 
-                // 방문 안 한 곳이면
-                if (count[next] == -1) {
-                    count[next] = count[now] + 1;
+                if(number[next] == 0){
                     q.add(next);
+                    number[next] = number[cur] + 1;
+                }
+
+                if(next == K){
+                    return;
                 }
             }
         }
