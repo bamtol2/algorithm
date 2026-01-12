05928e71f70fdcbd5d6c3d8b6d9498ecf5af676f
seungwoo <fatmunk7504@gmail.com>
2026-01-12 23:13:26 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 120 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,12 +1,12 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int t,n;
-    static int[][] pos;
+    static int[][] songdo;
     static boolean[] visited;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -14,25 +14,24 @@ public class Main {
 
         for (int tc = 0; tc < t; tc++){
             n = Integer.parseInt(br.readLine());
-
-            pos = new int[n+2][2];
+            songdo = new int[n+2][2];
             visited = new boolean[n+2];
 
-            // 좌표 입력
-            for (int i = 0; i < n + 2; i++){
+            for (int i = 0; i < n+2; i++){
                 st = new StringTokenizer(br.readLine());
-                pos[i][0] = Integer.parseInt(st.nextToken());
-                pos[i][1] = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                songdo[i][0] = a;
+                songdo[i][1] = b;
             }
-
             if (bfs()){
                 System.out.println("happy");
             } else {
                 System.out.println("sad");
             }
+
         }
     }
-
     static boolean bfs(){
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
@@ -42,20 +41,20 @@ public class Main {
             int cur = q.poll();
 
             // 페스티벌 도착
-            if (cur == n + 1){
+            if (cur == n+1){
                 return true;
             }
 
-            for (int i = 0; i < n+2; i++){
+            for (int i = 1; i < n+2; i++){
                 if (!visited[i]){
-                    int dist = Math.abs(pos[cur][0] - pos[i][0])
-                            + Math.abs(pos[cur][1] - pos[i][1]);
+                    int dist = Math.abs(songdo[cur][0] - songdo[i][0]) + Math.abs(songdo[cur][1] - songdo[i][1]);
 
                     if (dist <= 1000){
                         visited[i] = true;
                         q.add(i);
                     }
                 }
+
             }
         }
         return false;
