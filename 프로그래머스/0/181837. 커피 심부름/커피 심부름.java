0698d8c3882849b315a3e0552e5516a761fcde92
seungwoo <fatmunk7504@gmail.com>
2026-02-08 14:22:58 +0900

    [level 0] Title: 커피 심부름, Time: 0.74 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int sum = 0;
+
+        for (String s : order) {
+            if (s.contains("americano") || s.equals("anything")) {
+                sum += 4500;
+            } else {
+                sum += 5000;
+            }
+        }
+
+        return sum;
+    }
+}
