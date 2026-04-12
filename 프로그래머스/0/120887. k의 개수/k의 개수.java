98925c0a1bbf97c9cd726117578070cfd6fbbb25
seungwoo <fatmunk7504@gmail.com>
2026-04-12 09:47:43 +0900

    [level 0] Title: k의 개수, Time: 19.72 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        char target = (char)(k + '0');
+        
+        for (int num = i; num <= j; num++) {
+            String str = String.valueOf(num);
+            for (int idx = 0; idx < str.length(); idx++) {
+                if (str.charAt(idx) == target) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
